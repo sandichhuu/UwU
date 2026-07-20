7441ff0aeb8b01d325d663fd27cde302ab33298e
sandichhuu <sandichhuu@gmail.com>
2026-07-21 06:47:06 +0700

    A* Grid on Unity

--- UnityProject/Assets/UwU/PathFinding/AStar/AStarGridBehaviour.cs
+++ UnityProject/Assets/UwU/PathFinding/AStar/AStarGridBehaviour.cs
@@ -4,6 +4,70 @@ namespace UwU.PathFinding.AStar
 {
     public class AStarGridBehaviour : MonoBehaviour
     {
+        [SerializeField] private int width = 64;
+        [SerializeField] private int height = 64;
+        [SerializeField] private int startIndex = 64;
+        [SerializeField] private int targetIndex = 64;
 
+        private Node[] grid;
+
+        private void Start()
+        {
+            if (ValidateInput())
+            {
+                SetupGrid();
+                SetupObstacles();
+                Solve();
+            }
+            else
+            {
+                Debug.Log("Invalid input !");
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            var size = this.width * this.height;
+            return this.startIndex < size && this.targetIndex < size;
+        }
+
+        private void Solve()
+        {
+            var path = AStarGrid.FindPath(this.grid, this.width, this.height, this.startIndex, this.targetIndex);
+
+            if (path != null)
+            {
+                Debug.Log("Found Solution:");
+                foreach (var node in path)
+                {
+                    Debug.Log($"-> ({node.X}, {node.Y})");
+                }
+            }
+            else
+            {
+                Debug.Log("Not found path");
+            }
+        }
+
+        private void SetupObstacles()
+        {
+            this.grid[1 * this.width + 2].IsWalkable = false;
+            this.grid[2 * this.width + 2].IsWalkable = false;
+            this.grid[3 * this.width + 2].IsWalkable = false;
+        }
+
+        private void SetupGrid()
+        {
+            this.grid = new Node[this.width * this.height];
+
+            for (var y = 0; y < this.height; y++)
+            {
+                for (var x = 0; x < this.width; x++)
+                {
+                    var index = y * this.width + x;
+                    this.grid[index] = new Node(x, y, true);
+                }
+            }
+        }
     }
 }
