91d152e7d7b8c43b6e3be58d675cc3fc91cd0bf4
sandichhuu <sandichhuu@gmail.com>
2026-07-25 16:41:39 +0700

    add function check blocking path or not, add function get next step (from current location).

--- UnityProject/Assets/UwU/PathFinding/FlowField/FlowFieldGridMapBehaviour.cs
+++ UnityProject/Assets/UwU/PathFinding/FlowField/FlowFieldGridMapBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UwU.Grid;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 namespace UwU.PathFinding.FlowField
 {
@@ -79,9 +80,85 @@ namespace UwU.PathFinding.FlowField
             new(1, 0)
         };
 
-        private bool IsValidPosition(Vector2Int pos)
+        public bool IsValidPosition(Vector2Int pos)
         {
             return pos.x >= 0 && pos.x < this.width && pos.y >= 0 && pos.y < this.height;
+        }
+
+        /// <summary>
+        /// Try to set location is obstacle but not blocking path
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="isObstacle"></param>
+        /// <returns></returns>
+        public bool TestAndSetObstacle(Vector2Int pos, bool isObstacle)
+        {
+            if (!IsValidPosition(pos)) 
+                return false;
+
+            if (IsStartOrTarget(pos)) 
+                return false;
+
+            var gridMap = GetGridMap();
+            var previousState = gridMap[pos].IsObstacle;
+            gridMap[pos].IsObstacle = isObstacle;
+
+            Compute();
+
+            var allStartsValid = true;
+            foreach (var start in this.startCells)
+            {
+                if (gridMap[start].distance == -1)
+                {
+                    allStartsValid = false;
+                    break;
+                }
+            }
+
+            if (!allStartsValid)
+            {
+                gridMap[pos].IsObstacle = previousState;
+                Compute();
+                return false;
+            }
+
+            return true;
+        }
+
+        public Vector2Int GetNextStep(Vector2Int currentLocation)
+        {
+            if (!IsValidPosition(currentLocation)) 
+                return currentLocation;
+
+            var gridMap = GetGridMap();
+            var currentDist = gridMap[currentLocation].distance;
+
+            if (currentDist <= 0) 
+                return currentLocation;
+
+            var bestNextCell = currentLocation;
+            var minDistance = currentDist;
+
+            foreach (var dir in Directions)
+            {
+                var neighbor = currentLocation + dir;
+                if (IsValidPosition(neighbor))
+                {
+                    var neighborDist = gridMap[neighbor].distance;
+                    if (neighborDist != -1 && neighborDist < minDistance)
+                    {
+                        minDistance = neighborDist;
+                        bestNextCell = neighbor;
+                    }
+                }
+            }
+
+            return bestNextCell;
+        }
+
+        public bool IsStartOrTarget(Vector2Int pos)
+        {
+            return this.startCells.Contains(pos) || this.targetCells.Contains(pos);
         }
     }
 }
